08121a6ec9a7ce28515663ccca87a7a4bce9b816
j3w7 <jens.wissmann@gmx.net>
2013-04-22 18:03:20 +0200

    - maven convention: src/*/java -> src/*/scala - pom configured for maven build - weekday trainer

--- src/main/scala/spells/WeekDaySpellBenjamin.scala
+++ src/main/scala/spells/WeekDaySpellBenjamin.scala
@@ -16,7 +16,7 @@ object WeekDaySpellBenjamin {
 
   def cv(y: Int) = Map(
     16 -> 0, 17 -> 5, 18 -> 3, 19 -> 1,
-    20 -> 0, 21 -> 5)(y)
+    20 -> 0, 21 -> 5)(cc(y))
 
   // TODO check why this is not working as (cvsc2)
   def cvc(cc: Int, m: Int) =
@@ -25,14 +25,23 @@ object WeekDaySpellBenjamin {
   def cvc2(y: Int, m: Int) =
     if (new LocalDate(y, m, 1).year().isLeap() && (m == 1 || m == 2)) -1 else 0
 
-  def r(dy: Int) = abs(dy / 4) + dy
+  def r(y: Int) = abs(dy(y) / 4) + dy(y)
+
+  def yv(y: Int, m: Int) = cv(y) + cvc2(y, m)
+
+  /**
+    * century (first two digits of yyyy)
+    */
+  def cc(y: Int) = y.toString.substring(0, 2).toInt
+
+  /**
+    * decade (last two digits of yyyy)
+    */
+  def dy(y: Int) = y.toString.substring(2, 4).toInt
 
   def nameOfDay(d: Int, m: Int, y: Int): Int = {
-    val cc = y.toString.substring(0, 2).toInt
-    val dy = y.toString.substring(2, 4).toInt
-
     //cvc(cc, m)
-    return (d + mv(m) + cv(cc) + cvc2(y, m) + r(dy)) % 7
+    return (d + mv(m) + cv(y) + cvc2(y, m) + r(y)) % 7
   }
 
 }
--- src/main/scala/trainer/datetime/DataTrainers.scala
+++ src/main/scala/trainer/datetime/DataTrainers.scala
@@ -2,16 +2,18 @@ package trainer.datetime
 
 import java.util.Locale.GERMAN
 import java.util.Locale.GERMANY
-
+import scala.collection.immutable.HashSet
+import scala.math.abs
 import org.joda.time.format.DateTimeFormat
-
 import spells.WeekDaySpellBenjamin.cv
+import spells.WeekDaySpellBenjamin.cvc2
 import spells.WeekDaySpellBenjamin.mv
 import spells.WeekDaySpellBenjamin.r
 import utils.CalendarUtils.genDate
 import utils.CalendarUtils.getMonthName
 import utils.CalendarUtils.getWeekDay
 import utils.RandomUtils
+import org.joda.time.DateTime
 
 object DateTrainingsLoop extends App {
 
@@ -22,7 +24,7 @@ object DateTrainingsLoop extends App {
     val myAnswer = readLine
 
     val weekday = getWeekDay(dt, GERMAN)
-//    val msg = if (myAnswer == weekday) "ok" else "no, it's a " + weekday
+    //    val msg = if (myAnswer == weekday) "ok" else "no, it's a " + weekday
     println(weekday)
     println
 
@@ -86,14 +88,13 @@ object DivFourTrainingsLoop extends App {
     println(num)
     readLine
 
-    val s = Math.abs(num / 4)
+    val s = abs(num / 4)
 
     println(s + "\n")
 
   }
 
 }
-
 
 object DivFourModSevenTrainingsLoop extends App {
 
@@ -103,13 +104,34 @@ object DivFourModSevenTrainingsLoop extends App {
     print(num)
     readLine
 
-    val s = Math.abs(num / 4) % 7
+    val s = abs(num / 4) % 7
 
     println(s + "\n")
 
   }
-
 }
 
+object DaysForWeekdayLoop extends App {
 
+  while (true) {
+
+    val wn = RandomUtils.randBetween(0, 6)
+    val m = RandomUtils.randBetween(1, 12)
+    val y = RandomUtils.randBetween(1600, 2099)
+    print(getWeekDay(wn, GERMAN) + " " + m + " " + y)
+    readLine
+
+    val offs = (wn + mv(m) + cv(y) + r(y) + cvc2(y, m)) % 7
+
+    val days = for (
+      i ← 0 to 4;
+      d ← List(i * 7 + offs);
+      if d > 0 && d <= new DateTime(y, m, 1, 0, 0).dayOfMonth.getMaximumValue
+    ) yield (d)
+
+    println(days + "\n")
+
+  }
+
+}
 
